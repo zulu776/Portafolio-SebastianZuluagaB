52f3c6802736e73f6479c3902e7783d4aebce912
zulu776 <zuluaga776@gmail.com>
2021-08-11 21:26:00 -0500

    scroll actualizacion

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,1 +1,12 @@
-alert("Si Funciona");
+const header = document.getElementById("main-header")
+
+function scrollHeaderTop(){
+    if(window.scrollY > 500){
+        header.classList.add("fixed-header")
+    }
+    console.log(window.scrollY)
+}
+
+// document.addEventListener("scroll",scrollHeaderTop);
+document.onscroll = scrollHeaderTop;
+
