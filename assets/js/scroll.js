5b1efff5954b83a4b99da2840668d7ca00a0e10c
zulu776 <zuluaga776@gmail.com>
2021-08-12 21:57:26 -0500

    actualizacion JS

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,12 +1,24 @@
 const header = document.getElementById("main-header")
+const thirdSection = document.getElementById("third-section")
+const about = document.getElementById("about")
 
 function scrollHeaderTop(){
+
+    let offSetAbout = thirdSection.offsetTop - 200;
+    // console.log("Offset --->",offSetAbout - 100);
+    console.log("Original --->",offSetAbout)
+
     if(window.scrollY > 500){
         header.classList.add("fixed-header")
     }
-    console.log(window.scrollY)
+    // console.log(window.scrollY)
+
+    if(thirdSection.offsetTop === window.scrollY){
+        about.style.opacity = "1"
+    }
 }
 
 // document.addEventListener("scroll",scrollHeaderTop);
 document.onscroll = scrollHeaderTop;
+
 
